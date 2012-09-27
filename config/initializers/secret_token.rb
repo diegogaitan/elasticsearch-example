d497fa9c131b46f34caa1266ce5d923b95640a27
Diego Gaitán <diego.gaitan@gmail.com>
2012-09-27 11:15:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TireApp::Application.config.secret_token = '8413d17e65e126708ad0bf6af420c96af6f5bf312283c36c7465543efcf07a6b0914f34e780309e9c6627249fc2a3758dd9ffaffb5cf6d3c9557bc728385998f'
