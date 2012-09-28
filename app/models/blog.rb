45df2922597de8d1e20f3e05c9c0529d5c09e3d3
Diego Gaitán <diego.gaitan@gmail.com>
2012-09-27 21:12:52 -0500

    Added facets for tags

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -13,15 +13,29 @@ class Blog < ActiveRecord::Base
     indexes :tag_list, type: 'string', analyzer: 'keyword'
   end
 
+  def self.tags_to_array(tags)
+    the_tags = Set.new
+
+    [' ', ','].each do |s|
+      the_tags |= tags.split(s).map { |tag| tag.strip }
+    end
+
+    the_tags.to_a
+  end
+
   def self.search(params)
     tire.search(load: true, page: params[:page], per_page: 2) do
       query do
         boolean do
           must { range :created_at, lte: Time.zone.now }
           must { string params[:query] } if params[:query].present?
+          must { terms :tag_list, Blog.tags_to_array(params[:the_tag]) } if params[:the_tag].present?
         end
       end
       sort { by :created_at, 'desc' } if params[:query].blank?
+      facet 'tags' do
+        terms :tag_list
+      end
     end
   end
 
